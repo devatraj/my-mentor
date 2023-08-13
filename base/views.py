44a1c43ffcd69bf7609122d7f848a9271cb5e385
unknown <idevatraj@gmail.com>
2023-08-13 20:58:24 +0530

    initial with CRUD

---
+++ base/views.py
@@ -0,0 +1,47 @@
+from lib2to3.pytree import Node
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Room
+from .forms import RoomForm
+# Create your views here.
+
+
+
+def home(request):
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room=Room.objects.get(id=pk)
+    context={'room':room}
+    return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form=RoomForm()
+    if request.method=='POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
+
+
+def updateRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    form=RoomForm(instance=room)
+    if request.method=='POST':
+        form=RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
